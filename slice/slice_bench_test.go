7a0410b4f7f05fd6d95ee7c959b968ed61aefbc5
Martin Angers <martin.n.angers@gmail.com>
2021-06-16 10:02:46 -0400

    Benchmark the Prepend function

---
+++ slice/slice_bench_test.go
@@ -0,0 +1,22 @@
+package slice
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkPrepend(b *testing.B) {
+	for _, n := range []int{1, 10, 100, 1000, 10000, 100000, 1000000} {
+		b.Run(fmt.Sprintf("n=%d", n), func(b *testing.B) {
+			vals := sortedSlice(n, 1)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				newVals := Prepend(vals, 1)
+				if len(newVals) != len(vals)+1 {
+					b.Fatalf("want new len %d, got %d", len(vals)+1, len(newVals))
+				}
+			}
+		})
+	}
+}
